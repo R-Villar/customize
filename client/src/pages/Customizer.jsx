0fcd5b8c462c5f545eefea5f0e221c44da1413ab
Remberto Villar <rembertovillar@gmail.com>
2023-08-22 13:16:43 -0400

    add functions for filepicker.

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence, motion } from "framer-motion"
 import { useSnapshot } from "valtio"
 import config from "../config/config"
 import state from "../store"
-import { download } from "../assets"
+import { download, swatch } from "../assets"
 import { downloadCanvasToImage, reader } from "../config/helpers"
 import { EditorTabs, FilterTabs, DecalTypes } from "../config/constants"
 import { fadeAnimation, slideAnimation } from "../config/motion"
@@ -11,6 +11,58 @@ import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from "../compone
 
 export const Customizer = () => {
 	const snap = useSnapshot(state)
+
+	const [file, setFile] = useState("")
+
+	const [prompt, setPrompt] = useState("")
+	const [generatingImg, setGeneratingImg] = useState("")
+	const [activeEditorTab, setActiveEditorTab] = useState("")
+	const [activeFilterTab, setActiveFilterTab] = useState({ logoShirt: true, stylishShirt: false })
+
+	// show tab content depending on the activeTab
+	const generateTabContent = () => {
+		switch (activeEditorTab) {
+			case "colorpicker":
+				return <ColorPicker />
+			case "filepicker":
+				return <FilePicker file={file} setFile={setFile} readFile={readFile} />
+			case "aipicker":
+				return <AIPicker />
+
+			default:
+				return null
+		}
+	}
+
+	const handleDecals = (type, result) => {
+		const decalType = DecalTypes[type]
+		state[decalType.stateProperty] = result
+
+		if (!activeFilterTab[decalType.filterTab]) {
+			handleActiveFilterTab(decalType.filterTab)
+		}
+	}
+
+	const handleActiveFilterTab = (tabName) => {
+		switch (tabName) {
+			case "logoShirt":
+				state.isLogoTexture = !activeFilterTab[tabName]
+				break
+			case "stylishShirt":
+				state.isFullTexture = !activeFilterTab[tabName]
+			default:
+				state.isLogoTexture = true
+				state.isFullTexture = false
+		}
+	}
+
+	const readFile = (type) => {
+		reader(file).then((result) => {
+			handleDecals(type, result)
+			setActiveEditorTab("")
+		})
+	}
+
 	return (
 		<AnimatePresence>
 			{!snap.intro && (
@@ -19,8 +71,13 @@ export const Customizer = () => {
 						<div className='flex items-center min-h-screen'>
 							<div className='editortabs-container tabs'>
 								{EditorTabs.map((tab) => (
-									<Tab key={tab.name} tab={tab} handleClick={() => {}} />
+									<Tab
+										key={tab.name}
+										tab={tab}
+										handleClick={() => setActiveEditorTab(tab.name)}
+									/>
 								))}
+								{generateTabContent()}
 							</div>
 						</div>
 					</motion.div>
